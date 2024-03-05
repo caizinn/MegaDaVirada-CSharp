ab08805866a650ae939b1cd9630087d9fa9312a1
caizinn <caiobrasil18@hotmail.com>
2024-03-04 21:07:59 -0300

    Generate random numbers with C#

---
+++ NumeroMega/Entities/VerificarNumero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumeroMega.Entities
+{
+    internal class VerificarNumero
+    {
+        public List<int> GerarNumero(int numeroTotal)
+        {
+            var aleatorio = new Random();
+            List<int> listaNumeros = new List<int>();
+            int numero;
+
+            for (int i = 1; i <= numeroTotal; i++)
+            {
+                do
+                {
+                    numero = aleatorio.Next(1, 61);
+                } while (listaNumeros.Contains(numero));
+                listaNumeros.Add(numero);
+                Console.WriteLine(numero);
+            }
+            return listaNumeros;
+        }
+    }
+}
---
+++ NumeroMega/Program.cs
@@ -0,0 +1,79 @@
+﻿using NumeroMega.Entities;
+using System;
+
+namespace NumeroMega
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            VerificarNumero numero = new VerificarNumero();
+            bool escolheuSair = false;
+
+            while (!escolheuSair)
+            {
+                //basta marcar de 6 a 20 números dos 60 disponíveis
+                Console.WriteLine("Mega da Virada - Boa sorte!");
+                Console.WriteLine("1- JOGAR\n2- SAIR");
+
+                try
+                {
+                    Console.Write("Informe a opção: ");
+                    int opcaoEscolhida = int.Parse(Console.ReadLine());
+
+                    if (opcaoEscolhida > 0 && opcaoEscolhida < 3)
+                    {
+                        switch (opcaoEscolhida)
+                        {
+                            case 1:
+                                Console.Write("Informe quantos números irá jogar: ");
+                                int qtdNumeros = int.Parse(Console.ReadLine());
+
+                                if (qtdNumeros > 5 && qtdNumeros < 21)
+                                {
+                                    numero.GerarNumero(qtdNumeros);
+                                    Console.WriteLine("\nPressione ENTER para continuar.");
+                                    Console.ReadLine();
+                                    Console.Clear();
+                                }
+                                else
+                                {
+                                    Console.Clear();
+                                    Console.WriteLine("Informe um valor correto!");
+                                    Console.WriteLine("Obs: Forneça de 6 a 20 números.");
+                                    Console.WriteLine("\nPressione ENTER para continuar.");
+                                    Console.ReadLine();
+                                    Console.Clear();
+                                }
+                                break;
+                            case 2:
+                                escolheuSair = true;
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n[OPÇÃO INVÁLIDA!]");
+                        Console.WriteLine("\nPressione ENTER para continuar.");
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("\nUm erro ocorreu: " + e.Message);
+                    Console.WriteLine("\nPressione ENTER para continuar.");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Erro inesperado: " + e.Message);
+                    Console.WriteLine("\nPressione ENTER para continuar.");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
